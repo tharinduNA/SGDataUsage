8933ad37f3b1b86e70b882976657266b0d78aca0
tharinduNA <tharindutna@gmail.com>
2019-08-27 11:38:39 +0800

    codables and response handler added

---
+++ SGDataUsage/SGDataUsage/Network/Codables.swift
@@ -0,0 +1,25 @@
+//
+//  Codables.swift
+//  SGDataUsage
+//
+//  Copyright © 2019 Tharindu Abeyratne. All rights reserved.
+//
+
+import Foundation
+
+
+struct DataUsage: Codable {
+    let help: String?
+    let success: Bool?
+    let result: Records?
+}
+
+struct Records: Codable {
+    let records: [QuaterDetails?]
+}
+
+struct QuaterDetails: Codable {
+    let volume_of_mobile_data: String?
+    let quarter: String?
+    let _id: Int?
+}
